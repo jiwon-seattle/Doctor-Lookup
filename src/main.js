b234156ace088edc3e2628080732575eb18c18a9
Jiwon Han <jiwon1.han@gmail.com>
2020-02-14 13:35:24 -0800

    Attempt to fetch API

--- src/main.js
+++ src/main.js
@@ -2,23 +2,30 @@ import $ from 'jquery';
 import 'bootstrap' ;
 import 'bootstrap/dist/css/bootstrap.min.css' ;
 import './styles.css' ;
-import { Doctor } from './doctor-service-service.js';
+// import { Doctor } from './doctor-service-service.js';
 
 $(document).ready(function() {
   $('#findtheDoctor').click(function() {
-    let doctor = new doctor
-    let response = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2018-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + ${process.env.API_KEY}`
-    (async () => {
-      const response = await dinoipsum.getDinoName();
-      getElements(response);
-    })();
-    function getElements(response) {
-      let letters = dinoipsum.getDinoLetterArray(response)
-      let length = dinoipsum.getDinoLength(response);
-      $('#showDino').text(`${response}`);
-      $('#dinoLength').text(`${length}`);
-      $('#dinoLetters').text(`${letters}`);
-    }
-});
+
+    let promise = new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2018-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=5&user_key=${process.env.API_KEY}`
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+
+    request.open("GET", url, true);
+    request.send();
+  });
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      $('#doctorNameResult').text(data);
+    }, function(error) {
+      $('#doctorNameResult').text(`${error.message}`)
+    });
+  });
 });
