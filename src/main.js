274d2ab71bde14d44190eee811bc93145e7a1be9
Jiwon Han <jiwon1.han@gmail.com>
2020-02-14 12:37:23 -0800

    Create json

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import $ from 'jquery';
-import 'bootstrap' ; 
-import 'bootstrap/dist/css/bootstrap.min.css' ; 
+import 'bootstrap' ;
+import 'bootstrap/dist/css/bootstrap.min.css' ;
 import './styles.css' ;
-import { Dinoipsum } from './weather-service.js';
+import { Doctor } from './doctor-service-service.js';
 
 $(document).ready(function() {
-  let dinoipsum = new Dinoipsum();
-  $('#getDinoText').click(function() {
-    // console.log("hello");
-
+  $('#findtheDoctor').click(function() {
+    let doctor = new doctor
+    let response = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2018-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + ${process.env.API_KEY}`
     (async () => {
-      const response = await dinoipsum.getDinoName();  
+      const response = await dinoipsum.getDinoName();
       getElements(response);
     })();
     function getElements(response) {
@@ -19,6 +19,6 @@ $(document).ready(function() {
       $('#showDino').text(`${response}`);
       $('#dinoLength').text(`${length}`);
       $('#dinoLetters').text(`${letters}`);
-    }   
+    }
 });
 });
