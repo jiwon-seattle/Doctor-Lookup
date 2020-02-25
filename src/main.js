216557b99316ae7c5bcaa1831de8235e1f067fe9
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-02-24 21:32:21 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,67 +1,114 @@
-import $ from 'jquery'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles.css'
-import { DoctorService } from './doctor-service'
+<!DOCTYPE html>
+<html>
+  <head>
+      <script type="text/javascript" src="main.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.6/handlebars.js">
+      </script>
+      <title>Doctor Lookup</title>
+    </head>
+    <body>
 
-$(document).ready(function() {
-  $('#searchDoctorName').click(function() {
-    $("#doctorIssue").toggle()
-    $("#doctorName").toggle()
-  })
+      <div class="container">
+      
+        <div class="item">  
+        <br>
+        <h1>&nbsp;Doctor Lookup</h1>
+        <br>
+        <br>
+        <h3>&nbsp;Search potential doctors in the city that you are looking for.</h3>
 
-  $('#searchIssue').click(function() {
-    $("#doctorIssue").toggle()
-    $("#doctorName").toggle()
-  })
+        <form id="doctorIssue">
+          <div class="form-row">
+            <div class="locationIssue">
+              <label for="location">&nbsp;&nbsp;A location:</label>
+              <input id="locationIssue" type="text" placeholder='E.g. "Seattle"' required>
+              &nbsp;
+            </div>
 
-  $('#doctorIssue').submit(function(event) {
-    event.preventDefault()
-    $("#doctorInformation").hide()
-    $("#doctorsInCity").show()
+            <div class="Issue">
+              <label for="Issue">A medical issue:</label>
+              <input type="text" id="issue" placeholder='E.g. "Dermatology"' required>
+              <button type="submit" class="btn-info">Find doctors</button>
+              <button id="searchDoctorName" type="button" style="font-size:1rem;">Search by doctor's last name?</button>
+            </div>
 
-    const city = $('#locationIssue').val()
-    const issue = $('#issue').val()
-
-    asynAPICall(issue)
-
-    async function asynAPICall(issue) {
-        let doctorService = new DoctorService()
-        const response = await doctorService.getDoctorByCity(city)
-        let lan = response.results[0].geometry.lat
-        let lng = response.results[0].geometry.lng
-        const doctorResponse = await doctorService.getDoctors(issue, lan, lng)
-        let doctors = doctorService.doctors(doctorResponse)
-        let doctorList = doctorService.doctorsProfile(doctorResponse)
-        doctors
-        doctorList
-
-      }
+          </div>
+        </form>
 
 
-  })
+          <form id="doctorName">
+            <div class="form-row">
+              <div class="locationDoctor">
+                <label for="location">&nbsp;&nbsp;A location:</label>
+                <input class="form-control-md" id="locationDoctor" type="text" placeholder='E.g. "LA"' required>
+              </div>
 
-  $('#doctorName').submit(function(event) {
-    event.preventDefault()
-    $("#doctorsInCity").hide()
-    $("#doctorInformation").show()
+              <div class="doctorName">
+                <label for="doctorName">&nbsp;Doctor's last name:</label>
+                <input class="form-control-md" type="text" id="name" placeholder='E.g. "Smith"' required>
+                <button type="submit" class="btn-info">Find doctors</button>
+                <button id="searchIssue" type="button" style="font-size:1rem;">Search by medical issue?</button>
+              </div>
 
-    const city = $('#locationDoctor').val()
-    const name  = $('#name').val()
-    asynAPICall(name)
+            </div>
+          </form>
+        </div>
+        
+        <div class="item">
+          &nbsp;&nbsp;
+          <img id="doctor" src="https://cdn.pixabay.com/photo/2017/09/27/20/55/clinical-trial-icon-2793430_1280.png">
+        </div>
+        
+        <div class="break">
+          <div id="doctorsInCity"></div>
+          <script id="doctors-template" type="text/x-handlers-template">
+            <h4>Better Doctor - Doctor Search Results</h4>
+            <table cellpadding="10" width="100%">
+              <thead>
+                <th>Name</th>
+                <th>Title</h5></th>
+                <th>specialties</th>
+              </thead>
+              <tbody>
+                {{#data}}
+                <tr>
+                  <td style="width:33%"><a id="{{uid}}" class="doctorsName">
+                  {{profile.first_name}} {{profile.middle_name}} {{profile.last_name}}</a>
+                  </td>
+                  <td style="width:33%">{{profile.title}}</td>
+                  <td style="width:33%">{{specialties.[0].name}}</td>
+                </tr>
+                {{/data}}
+              </tbody>
+            </table>
+          </script>
 
-    async function asynAPICall(name) {
-        let doctorService = new DoctorService()
-        const response = await doctorService.getDoctorByCity(city)
-        let lan = response.results[0].geometry.lat
-        let lng = response.results[0].geometry.lng
-        const doctorResponse = await doctorService.getDoctorInfo(name, lan, lng)
-        let doctorDetail = doctorService.doctorInfo(doctorResponse)
-        doctorDetail
+          <div id="doctorInformation"></div>
+          <script id="doctorInfo-template" type="text/x-handlers-template">
+            <h4>Better Doctor - Doctor Search Results</h4>
+            <table cellpadding="10" width="100%">
+              <thead>
+                <th>Picture</th>
+                <th>Doctor</th>
+                <th>Specialty</th>
+                <th>Bio</th>
+                <th>Address & Contact</th>
+              </thead>
+              <tbody>
+                {{#data}}
+                <tr>
+                  <td><img src="{{profile.image_url}}"></img></td>
+                  <td style="white-space:nowrap"> <strong>{{profile.title}}</strong> {{profile.first_name}} {{profile.middle_name}} {{profile.last_name}}</td>
+                  <td>{{specialties.[0].name}}</td>
+                  <td style="width:70%">{{profile.bio}}</td>
+                  <td style="white-space:nowrap">{{practices.[0].visit_address.street}} {{practices.[0].visit_address.city}} {{practices.[0].visit_address.zip}} {{practices.[0].visit_address.state}}{{practices.[0].phones.[0].phones.[0].[0]}}</td>
+                </tr>
+                {{/data}}
+              </tbody>
+            </table>
+        </script>
+        </div>
+      </div>  
 
-      }
-
-
-  })
-
-})
+    </body>
+</html>
