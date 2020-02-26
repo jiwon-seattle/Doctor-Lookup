4708cdb20916ae0e4c03ede96372406ed1f40888
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-02-25 22:38:49 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,62 +1,62 @@
-import $ from 'jquery';
-import 'bootstrap' ;
-import 'bootstrap/dist/css/bootstrap.min.css' ;
-import './styles.css' ;
-import { DoctorService } from './doctor-service';
+import $ from 'jquery'
+import 'bootstrap' 
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import './styles.css' 
+import { DoctorService } from './doctor-service'
 
 $(document).ready(function() {
 
   $('#searchDoctorName').click(function(){
-    $('#doctorName').toggle();
-    $('#doctorIssue').toggle();
+    $('#doctorName').toggle()
+    $('#doctorIssue').toggle()
   })
 
   $('#searchIssue').click(function(){
-    $('#doctorName').toggle();
-    $('#doctorIssue').toggle();
+    $('#doctorName').toggle()
+    $('#doctorIssue').toggle()
   })
 
   $('#doctorIssue').submit(function(event) {
-    event.preventDefault();
-    const city = $('#locationIssue').val();
-    const issue = $('#issue').val();
-    $("#doctorsInCity").show();
-    $("#doctorInformation").hide();
+    event.preventDefault()
+    const city = $('#locationIssue').val()
+    const issue = $('#issue').val()
+    $("#doctorsInCity").show()
+    $("#doctorInformation").hide()
 
-    cityIssueAPI(issue);
+    cityIssueAPI(issue)
 
     async function cityIssueAPI(issue) {
-        let doctorService = new DoctorService();
-        const response = await doctorService.getDoctorByCity(city);
+        let doctorService = new DoctorService()
+        const response = await doctorService.getDoctorByCity(city)
         let lan = response.results[0].geometry.lat
         let lng = response.results[0].geometry.lng
-        const doctorResponse = await doctorService.getDoctors(issue, lan, lng);
-        let doctors = doctorService.doctors(doctorResponse);
+        const doctorResponse = await doctorService.getDoctors(issue, lan, lng)
+        let doctors = doctorService.doctors(doctorResponse)
         let doctorList = doctorService.doctorsProfile(doctorResponse)
         doctorList
       }
-  });
+  })
 
     $('#doctorName').submit(function(event) {
-      event.preventDefault();
-      const city = $('#locationDoctor').val();
-      const name = $('#name').val();
-      $("#doctorsInCity").hide();
-      $("#doctorInformation").show();
+      event.preventDefault()
+      const city = $('#locationDoctor').val()
+      const name = $('#name').val()
+      $("#doctorsInCity").hide()
+      $("#doctorInformation").show()
 
-      cityNameAPI(name);
+      cityNameAPI(name)
 
       async function cityNameAPI(name) {
-          let doctorService = new DoctorService();
-          const response = await doctorService.getDoctorByCity(city);
+          let doctorService = new DoctorService()
+          const response = await doctorService.getDoctorByCity(city)
           let lan = response.results[0].geometry.lat
           let lng = response.results[0].geometry.lng
-          const doctorResponse = await doctorService.getDoctorInfo(name, lan, lng);
-          let doctors = doctorService.doctors(doctorResponse);
+          const doctorResponse = await doctorService.getDoctorInfo(name, lan, lng)
+          let doctors = doctorService.doctors(doctorResponse)
           let doctorInfo = doctorService.doctorInfo(doctorResponse)
           doctorInfo
         }
-    });
+    })
 
 
   });
